1e42a620cabd305b58782a86a97acb2d2ae43846
Phu Sang <sang.le@ninecode.vn>
2021-03-04 09:27:07 +0700

    add mock api

---
+++ src/mock-api/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var apiMocker = require('connect-api-mocker');
+ 
+var app = express();
+const PORT = 8080; 
+
+app.use('/api/messages', apiMocker('api/messages'));
+app.listen(PORT,()=>{
+    console.log(`Example app listening on port http://localhost:${PORT} !`)
+});
--- src/mock-api/index.ts
+++
@@ -1,6 +0,0 @@
-export const fakeRequest = (body: any) => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(body), 1500);
-    });
-  };
-  
