70efa046bb9f6980f96edd8322e9b92f96df046b
Phu Sang <sang.le@ninecode.vn>
2021-03-05 14:56:32 +0700

    fdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,43 @@
 //lib
-import React, { useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { I18nextProvider } from "react-i18next";
-import NProgress from "nprogress";
-import 'nprogress/nprogress.css'
-
+import React, { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 //components
-import { Div } from "@Common/TagAntdNotSupport/TagAntdNotSupport";
-import AppRouter from "@Utils/AppRoute/AppRoute";
+import { Div } from '@Common/TagAntdNotSupport/TagAntdNotSupport';
+import AppRouter from '@Utils/AppRoute/AppRoute';
 //config
-import i18next from "@Configs/i18n/i18n";
+import i18next from '@Configs/i18n/i18n';
 //style
-import "./app.scss";
-import "@StyleSheets/nprogress.scss"
+import './app.scss';
+import '@StyleSheets/nprogress.scss';
 //const
 //store
 import { Provider } from 'react-redux';
 import storeConfig from './reduxConfig/store';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 
-const { store, persistor } = storeConfig()
+const { store, persistor } = storeConfig();
 
 export default function App() {
   NProgress.start();
   useEffect(() => {
     NProgress.done();
-             return () => {
-    }
-  }, [])
+    return () => {};
+  }, []);
   return (
     <Div className="app">
       <I18nextProvider i18n={i18next}>
-                                        <BrowserRouter>
+        <BrowserRouter>
           <React.Suspense fallback={<div className="fallback" />}>
-                                   <Provider store={store}>
+            <Provider store={store}>
               <PersistGate loading={null} persistor={persistor}>
                 <AppRouter />
               </PersistGate>
             </Provider>
-                                   </React.Suspense>
+          </React.Suspense>
         </BrowserRouter>
       </I18nextProvider>
     </Div>
